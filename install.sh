1b608dfc580c88703f28a6f2a2cb94661fbb0827
Chiyang Wan <chiyang10000@gmail.com>
2018-12-14 06:05:53 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 case $1 in
     -f)
-	# rm -f ~/.bashrc
-	# rm -f ~/.bash_profile
 	rm -f ~/.vimrc
 	rm -rf ~/.lldbinit
         ;;
@@ -12,26 +10,21 @@ esac
 git config --global color.ui auto
 git config --global core.editor vim
 git config --global core.safecrlf true
+git config --global push.default current
 git config --global credential.helper store
-# git config --global push.default simple
-# git config --global core.autocrlf true
 
 rm -rf ~/yizhiyang/config/git-*
-curl -o ~/yizhiyang/config/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-curl -o ~/yizhiyang/config/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+curl -k -o ~/yizhiyang/config/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+curl -k -o ~/yizhiyang/config/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 
 rm -rf ~/yizhiyang/bin/cpplint.py
-curl -o ~/yizhiyang/bin/cpplint.py https://raw.githubusercontent.com/google/styleguide/gh-pages/cpplint/cpplint.py
+curl -k -o ~/yizhiyang/bin/cpplint.py https://raw.githubusercontent.com/google/styleguide/gh-pages/cpplint/cpplint.py
 chmod +x ~/yizhiyang/bin/cpplint.py
 
 ln -s ~/yizhiyang/config/.lldbinit ~/.lldbinit
-
 ln -s ~/yizhiyang/config/.vimrc ~/.vimrc
 mkdir -p ~/.vim
 cp ~/yizhiyang/config/cpp.vim ~/.vim/
-
-# ln -s ~/yizhiyang/config/.bashrc ~/.bashrc
-# ln -s ~/.bashrc ~/.bash_profile
 
 echo 'source $HOME/yizhiyang/config/.bashrc' >> ~/.bashrc
 
