2a22a8a03757b7114c00ea39dc979cb8029c0627
Chiyang Wan <chiyang10000@gmail.com>
2019-06-25 19:58:10 +0800

    Update copy tpch

--- sql/init_tpchschema.copy.sql
+++ sql/init_tpchschema.copy.sql
@@ -1,12 +1,12 @@
 \timing
-copy nation from '/Users/admin/Documents/dataSet/tpch1g/nation/nation.tbl' delimiter '|';
-copy region  from '/Users/admin/Documents/dataSet/tpch1g/region/region.tbl' delimiter '|';
-copy part from '/Users/admin/Documents/dataSet/tpch1g/part/part.tbl' delimiter '|';
-copy supplier from '/Users/admin/Documents/dataSet/tpch1g/supplier/supplier.tbl' delimiter '|';
-copy partsupp from '/Users/admin/Documents/dataSet/tpch1g/partsupp/partsupp.tbl' delimiter '|';
-copy customer from '/Users/admin/Documents/dataSet/tpch1g/customer/customer.tbl' delimiter '|';
-copy orders from '/Users/admin/Documents/dataSet/tpch1g/orders/orders.tbl' delimiter '|';
-copy lineitem from '/Users/admin/Documents/dataSet/tpch1g/lineitem/lineitem.tbl' delimiter '|';
+copy nation from '/db_data/tpch1g/nation/nation.tbl' delimiter '|';
+copy region  from '/db_data/tpch1g/region/region.tbl' delimiter '|';
+copy part from '/db_data/tpch1g/part/part.tbl' delimiter '|';
+copy supplier from '/db_data/tpch1g/supplier/supplier.tbl' delimiter '|';
+copy partsupp from '/db_data/tpch1g/partsupp/partsupp.tbl' delimiter '|';
+copy customer from '/db_data/tpch1g/customer/customer.tbl' delimiter '|';
+copy orders from '/db_data/tpch1g/orders/orders.tbl' delimiter '|';
+copy lineitem from '/db_data/tpch1g/lineitem/lineitem.tbl' delimiter '|';
 create view revenue0 (supplier_no, total_revenue) as
         select
                 l_suppkey,
