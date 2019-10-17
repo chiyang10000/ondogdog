9297c14985cd178bf6d828aee487f2bd6e0c15ea
Chiyang Wan <chiyang10000@gmail.com>
2019-10-17 09:31:49 +0800

    refactor

--- install.sh
+++ install.sh
@@ -19,19 +19,28 @@ git config --global credential.helper store
 git config --global blame.date short
 
 rm -rf ~/yizhiyang/config/git-*
-curl -k -o ~/yizhiyang/config/git-completion.bash https://raw.githubusercontent.com/git/git/v2.17.0/contrib/completion/git-completion.bash
-curl -k -o ~/yizhiyang/config/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+version=`git --version | cut -d' ' -f3`
+major_version=`cut -d. -f1 <(echo $version)`
+minor_version=`cut -d. -f2 <(echo $version)`
+if [[ $major_version -ge 2 && $minor_version -gt 9 ]]; then
+  tag=v$version
+else
+  tag=master
+fi
+curl -k -o ~/yizhiyang/config/git-completion.bash https://raw.githubusercontent.com/git/git/$tag/contrib/completion/git-completion.bash
+curl -k -o ~/yizhiyang/config/git-prompt.sh https://raw.githubusercontent.com/git/git/$tag/contrib/completion/git-prompt.sh
 
 rm -rf ~/yizhiyang/bin/cpplint.py
 curl -k -o ~/yizhiyang/bin/cpplint.py https://raw.githubusercontent.com/google/styleguide/gh-pages/cpplint/cpplint.py
 chmod +x ~/yizhiyang/bin/cpplint.py
 
-ln -s ~/yizhiyang/config/.lldbinit ~/.lldbinit
-ln -s ~/yizhiyang/config/.vimrc ~/.vimrc
+[[ -f ~/.lldbinit ]] || ln -s ~/yizhiyang/config/lldbinit ~/.lldbinit
+[[ -f ~/.vimrc ]] || ln -s ~/yizhiyang/config/vimrc ~/.vimrc
 mkdir -p ~/.vim
 cp ~/yizhiyang/config/cpp.vim ~/.vim/
 
-echo 'source $HOME/yizhiyang/config/.bashrc' >> ~/.bashrc
+echo 'source $HOME/yizhiyang/config/bashrc' >> ~/.bashrc
 
 [[ -f ~/.bash_profile ]] || ln -s ~/.bashrc ~/.bash_profile
+set +x
 source ~/.bashrc
