dc5781ed09e933fc51730ac5d1310aa4251f9310
Chiyang Wan <chiyang10000@gmail.com>
2019-10-23 12:53:06 +0800

    fix

--- bin/lib/yizhiyang.py
+++ bin/lib/yizhiyang.py
@@ -34,6 +34,18 @@ def get_git_new_lines(git_repo_dir):
     return new_line_set
 
 
+def print_line_set(new_line_set, report_line_set):
+    for file_name in report_line_set:
+        print('')
+        print("  {}".format(file_name))
+        last_line_num = -1
+        for line_num in sorted(report_line_set[file_name]):
+            if last_line_num + 1 != line_num:
+                print('')
+            print("      {}\t {}".format(line_num, new_line_set[file_name][line_num]))
+            last_line_num = line_num
+
+
 def print_coverage(new_line_set, covered_line_set, uncovered_line_set):
     uncovered_line_count = sum(map(lambda x: len(x), uncovered_line_set.values()))
     covered_line_count = sum(map(lambda x: len(x), covered_line_set.values()))
@@ -50,20 +62,11 @@ def print_coverage(new_line_set, covered_line_set, uncovered_line_set):
 
     print('')
     print("Covered {} C++ library lines: ".format(covered_line_count))
-    for file_name in covered_line_set:
-        # continue
-        print('')
-        print("  {}".format(file_name))
-        for line_num in sorted(covered_line_set[file_name]):
-            print("      {}\t {}".format(line_num, new_line_set[file_name][line_num]))
+    print_line_set(new_line_set, covered_line_set)
 
     print('')
     print("Uncovered {} C++ library lines: ".format(uncovered_line_count))
-    for file_name in uncovered_line_set:
-        print('')
-        print("  {}".format(file_name))
-        for line_num in uncovered_line_set[file_name]:
-            print("      {}\t {}".format(line_num, new_line_set[file_name][line_num]))
+    print_line_set(new_line_set, uncovered_line_set)
 
     print('')
     print("Unit Test Code Coverage For Added reachable C++ lines: {} %".format(
