ac7174ed1c824b288b7f2a96d53aa10d7819eed7
Chiyang Wan <chiyang10000@gmail.com>
2020-08-09 21:04:24 +0800

    add junit-failure

---
+++ bin/junit-failure.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+import xml.etree.ElementTree as et
+import os.path
+
+if __name__ == '__main__':
+    assert (len(sys.argv) == 2)
+    assert (os.path.isfile(sys.argv[1]))
+    file_name = sys.argv[1]
+    tree = et.parse(file_name)
+    root = tree.getroot()
+
+    for testsuite in root.findall(".//testsuite"):
+        for testcase in testsuite.findall("./testcase[failure]"):
+            print("{}.{}".format(testsuite.attrib["name"],
+                                 testcase.attrib["name"]))
