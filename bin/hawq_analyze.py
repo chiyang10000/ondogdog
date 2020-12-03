267ad327e5372c11ac5b552d5581f3966a236947
Chiyang Wan <chiyang10000@gmail.com>
2020-12-03 13:30:11 +0800

    update tpch

--- bin/hawq_analyze.py
+++ bin/hawq_analyze.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python3
 from html.parser import HTMLParser
 from collections import Counter
+import argparse
 import requests
 import sys
 import os
 import re
 
 planchecker_url = os.getenv('planchecker_url', "http://cloudtest.oushu-tech.com:14122")
+
+
+def op_name(operator):
+    """
+        return a striped name
+    """
+    operator_pattern = re.compile(r'([A-Z][a-z]+ )+')
+    lines = operator.splitlines()
+
+    if 'Slice' in lines[0]:
+        op = lines[1].strip()
+    else:
+        op = lines[0].strip()
+
+    if operator_pattern.search(op):
+        op = (operator_pattern.search(op).group())
+    op = op.replace(' ', '')
+
+    return op
 
 
 class MyHTMLParser(HTMLParser):
@@ -24,9 +44,13 @@ class MyHTMLParser(HTMLParser):
         self.td = 0  # zero to mark non-table data
         self.curr_operator = ''
         self.curr_timing = 0.0
+
         self.operators = []
+        self.ops = []  # corresponding to operators with striped name
         self.timings = []
+
         self.new_executor = False
+        self.file_path = ''
 
     def handle_starttag(self, tag, attrs):
         if tag == 'tr':  # new row
@@ -40,6 +64,7 @@ class MyHTMLParser(HTMLParser):
             self.td = 0
             if self.curr_operator != '':  # skip invalid parsed row
                 self.operators.append(self.curr_operator)
+                self.ops.append(op_name(self.curr_operator))
                 self.timings.append(self.curr_timing)
                 # print(self.curr_timing)
                 # print(self.curr_operator)
@@ -58,6 +83,7 @@ class MyHTMLParser(HTMLParser):
 
 def parse(file):
     parser = MyHTMLParser()
+    parser.file_path = file
     data = [('action', 'parse')]
     files = {'uploadfile': open(file, 'rb')}
     plan_checker_response = requests.post(url=planchecker_url + "/plan/", files=files, data=data)
@@ -86,16 +112,7 @@ def parse_group_by_operator(query_num, query_file):
         if time < 0:  # skip dirty data
             continue
 
-        lines = res.operators[node_idx].splitlines()
-
-        if 'Slice' in lines[0]:
-            node = lines[1].strip()
-        else:
-            node = lines[0].strip()
-
-        if operator_pattern.search(node):
-            node = (operator_pattern.search(node).group())
-        node = node.replace(' ', '')
+        node = res.ops[node_idx]
 
         if node not in query_counter:
             query_counter[node] = 0
@@ -128,7 +145,7 @@ def split(file):
             output_stream.close()
             if is_plan:
                 ret.append(output_name_base + str(count) + '.txt')
-            count += 1
+                count += 1
             is_plan = False
             output_stream = open(output_name_base + str(count) + '.txt', 'w')
     if output_stream:
@@ -145,29 +162,49 @@ def compare(old_file, new_file):
     new_split_files = split(new_file)
     assert (len(old_split_files) == len(new_split_files))
 
+    mismatched_plans = []
+
     for spilt_file_idx in range(len(old_split_files)):
         # print(old_split_files[i], new_split_files[i])
         old_file = parse(old_split_files[spilt_file_idx])
         new_file = parse(new_split_files[spilt_file_idx])
 
-        assert (len(new_file.timings) == len(old_file.timings))
+        if len(new_file.timings) != len(old_file.timings):
+            # debug
+            # [print(i) for i in old_file.operators]
+            # [print(i) for i in new_file.operators]
+
+            mismatched_plans.append('Mismatched query plan {}:{} {}:{}'.format(
+                old_file.file_path, len(old_file.operators),
+                new_file.file_path, len(new_file.operators)))
+            continue
+
+        print('{}\n{}'.format(old_file.file_path, new_file.file_path))
+        print('-' * 80)
         for operator_idx in range(0, len(new_file.timings)):
             old_t = float(old_file.timings[operator_idx])
             new_t = float(new_file.timings[operator_idx])
-            if old_t > 30 and new_t > 30 and (old_t / new_t < 3):
+            if old_t > args.time and new_t > args.time and (old_t / new_t < args.speedup):
                 print(old_t, new_t)
                 print(old_file.operators[operator_idx])
                 print(new_file.operators[operator_idx])
 
+    print(mismatched_plans)
+
 
 def analyze(path):
-    assert (os.path.isdir(path))
+    assert (os.path.isdir(path) or os.path.isfile(path))
 
     query_num_matcher = re.compile(r'query([0-9]+)')
     tpch_query_num_matcher = re.compile(r'tpch_([0-9]+)')
 
     tot_counter = Counter()
-    query_files = [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+
+    query_idx = 0
+    if os.path.isdir(path):
+        query_files = [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+    else:
+        query_files = [path]
 
     print('{}\t{}\t{}'.format('QueryNo', 'Time', 'Operator', 'Mode'))
     for origin_query_file in query_files:
@@ -180,23 +217,42 @@ def analyze(path):
 
         split_files = split(origin_query_file)
         for single_query_file in split_files:
+            query_idx += 1
+            if os.path.isfile(path): query_num = str(query_idx)
             query_counter = parse_group_by_operator(query_num, single_query_file)
             tot_counter.update(query_counter)
 
     # Print group by tot_counter
-    exit()
+    if not args.verbose:
+        exit()
+
     print()
-    print('{}\t{}'.format('Time', 'Operator'))
+    tot_time = sum(map(lambda x: x[1], tot_counter.items()))
+    print('Total {}'.format(tot_time))
+
+    print('{}\t{}\t{}'.format('Time', 'Operator', 'Ratio'))
     for operator, time in sorted(tot_counter.items(), key=lambda x: x[1]):
-        print('{}\t{}'.format(time, operator))
+        print('{}\t{}\t{}%'.format(time, operator, round(100.0 * time / tot_time, 2)))
     exit()
 
 
 if __name__ == "__main__":
-    # Compare old vs new EXPLAIN ANALYZE output
-    if len(sys.argv) == 3:
-        compare(sys.argv[1], sys.argv[2])
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", help="print summary output of each operator",
+                        action="store_true")
+    parser.add_argument("-t", "--time", type=float, default=20.0,
+                        help="minimum timing counter in ms for single operator when checking old vs new (default: 20)")
+    parser.add_argument("-s", "--speedup", type=float, default=3.0,
+                        help="speedup requirement when checking old vs new (default: 3)")
+    parser.add_argument('input_args', metavar='path', type=str, nargs='+',
+                        help='old file vs new file, otherwise file/dir to be parsed')
+
+    args = parser.parse_args()
 
     # Analyze all EXPLAIN ANALYZE output
-    if len(sys.argv) == 2:
-        analyze(sys.argv[1])
+    if len(args.input_args) == 1:
+        analyze(args.input_args[0])
+
+    # Compare old vs new EXPLAIN ANALYZE output
+    if len(args.input_args) == 2:
+        compare(args.input_args[0], args.input_args[1])
--- sql/init_tpchschema.analyze.sql
+++ sql/init_tpchschema.analyze.sql
@@ -7,3 +7,11 @@ ANALYZE partsupp;
 ANALYZE customer ;
 ANALYZE orders;
 ANALYZE lineitem;
+
+SELECT nspname || '.' || relname AS "relation",
+       pg_size_pretty(pg_total_relation_size(C.oid)) AS "total_size"
+FROM pg_class C
+         LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace)
+WHERE nspname = current_schema()
+  AND C.relkind <> 'i'
+ORDER BY pg_total_relation_size(C.oid) DESC;
